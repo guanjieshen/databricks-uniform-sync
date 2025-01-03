aff09b2c5038eb705b836a6296c42e907a14abe6
Guanjie Shen <guanjie.shen@databricks.com>
2025-01-03 16:08:53 -0700

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="databricks_uniform_sync",
-    version="0.1.0",
+    version="0.2.0",
     description="A SDK for cloud compute pricing",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
