2fd5677fbad2a1fa8a12a9a8f971695d477a2b98
Guanjie Shen <guanjie.shen@databricks.com>
2025-03-26 15:03:07 -0500

    version clean up

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open("requirements.txt") as f:
 
 setup(
     name="databricks_uniform_sync",
-    version="0.1.0",
+    version="0.3.3",
     description="A SDK for syncing Databricks using Unity Catalog and Uniform",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
